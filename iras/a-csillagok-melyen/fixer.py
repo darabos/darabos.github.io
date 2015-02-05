4800091d202aaf8ea3af3185bdf4c215177879e3
Daniel Darabos <darabos.daniel@gmail.com>
2015-02-05 23:35:39 +0100

    Web rendering of the short story.

---
+++ iras/a-csillagok-melyen/fixer.py
@@ -0,0 +1,139 @@
+# coding=utf-8
+import pyphen
+import re
+
+hyp = pyphen.Pyphen(lang='hu_HU')
+def hyphenated(text):
+  res = []
+  for w in text.decode('cp1250').split(u' '):
+    res.append(hyp.inserted(w, u'&shy;'))
+  return u' '.join(res).encode('cp1250')
+
+lines = []
+prev = None
+separator = False
+heading = False
+body = False
+with file('a-csillagok-melyen.html') as f:
+  for l in f:
+    l = l.replace('<head><title></title>', u'<head><title>A csillagok mélyén</title>'.encode('cp1250'))
+    l = l.replace('&#x0151;', u'ő'.encode('cp1250')).replace('&#x0171;', u'ű'.encode('cp1250'))
+    l = l.replace('>&#x00A0;', '>')
+    if 'link rel="stylesheet"' in l:
+      l = (
+        '<meta name="viewport" content="width=device-width, initial-scale=1">\n'
+        '<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Lora:400,400italic,700" type="text/css">\n'
+        ) + l
+    if separator:
+      l = l.replace('"indent"', '"noindent"')
+    if l.strip() == 'class="pzdr-">&#x2726;</span>':
+      prev = prev.replace('"noindent"', '"separator"')
+      separator = True
+    else:
+      separator = False
+    if u'Vége'.encode('cp1250') in l:
+      l = l.replace('"noindent"', '"the-end"')
+    if '/body' in l:
+      l = '</div>\n' + l
+    if l.startswith('class="bchri8t-">') and prev.endswith('</span><span \n'):
+      prev = prev[:-len('</span><span \n')] + l[len('class="bchri8t-">'):]
+      continue
+    if prev and not heading:
+      if '<' not in prev and '>' not in prev:
+        prev = hyphenated(prev)
+      else:
+        def hypmiddle(m):
+          return m.group(1) + hyphenated(m.group(2)) + m.group(3)
+        prev = re.sub('(.*\>)(.*?)(\<.*)', hypmiddle, prev)
+      lines.append(prev)
+    prev = l
+    if not body and '<p' in l:
+      heading = True
+    if heading and not l.strip():
+      heading = False
+      body = True
+      lines.append(u'''
+<div class="container title">
+  <h1>A csillagok mélyén</h1>
+  <h2>Darabos Dániel, 2014</h2>
+</div>
+<div class="container story">
+      '''.strip().encode('cp1250'))
+lines.append(prev)
+
+with file('index.html', 'w') as f:
+  f.writelines(lines)
+
+with file('a-csillagok-melyen.css', 'w') as f:
+  f.write('''
+
+@media (min-width: 768px) {
+  .container {
+    max-width: 600px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+  .story {
+    font-size: 18px;
+  }
+}
+
+@media screen {
+  body {
+    background: rgb(240, 240, 240);
+    color: rgba(0, 0, 0, 0.6);
+  }
+}
+
+.story, .title {
+  font-family: 'Lora', serif;
+}
+
+.title {
+  text-align: center;
+  margin-top: 4em;
+  margin-bottom: 4em;
+}
+.title h1 {
+  margin-bottom: 0;
+}
+.title h2 {
+  font-size: 18px;
+  margin-top: 0;
+}
+
+.story {
+  text-align: justify;
+}
+
+.story p {
+  margin-top: 0;
+  margin-bottom: 0;
+  line-height: 1.4;
+}
+.story p.indent {
+  text-indent: 2em;
+}
+
+.story .separator,
+.story .the-end {
+  text-align: center;
+  margin-top: 2em;
+  margin-bottom: 2em;
+}
+
+.story .bchri8t- {
+  font-style: italic;
+}
+
+.story dl.description {
+  line-height: 1.4;
+}
+
+.story dt.description {
+  font-weight: bold;
+  margin-top: 1em;
+  margin-bottom: 0;
+}
+
+  '''.strip() + '\n')
